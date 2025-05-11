e93c85fb5d8bcd7591d196107f69f0df200a352d
Jingyong Ye <jyye21@m.fudan.edu.cn>
2025-05-11 22:03:39 +0800

    imagenet10

--- movear/hlmoetrain.sh
+++ movear/hlmoetrain.sh
@@ -1,9 +1,9 @@
 export PYTHONPATH=$PYTHONPATH:/private/yjy/project/MoVEAR
 
 torchrun --nproc_per_node=4 --nnodes=1 --node_rank=0 --master_addr="localhost" --master_port=29500 /private/yjy/project/MoVEAR/movear/models/hlmoetrain.py \
-  --depth=36 --saln=1 --pn=512 --bs=50 --ac=1 --ep=100 --tblr=4e-5 --fp16=1 --alng=5e-6 --wpe=0.01 --twde=0.08 \
+  --depth=16 --saln=1 --pn=256 --bs=50 --ac=1 --ep=350 --tblr=4e-5 --fp16=1 --alng=5e-6 --wpe=0.01 --twde=0.08 \
   --afuse=0 \
   --num_experts=4 --k=2 --noise_std=0.1 --aux_weight=0.05 \
   --theory_weight=0.01 \
-  --data_path="/private/yjy/project/MoVEAR/datasets/imagenet_organized" \
-  --local_out_dir_path="/private/yjy/project/MoVEAR/movear/hl_local_output"
+  --data_path="/private/yjy/project/MoVEAR/datasets/imagenet_subset_10" \
+  --local_out_dir_path="/private/yjy/project/MoVEAR/movear/d16_imagenet_256_hl"
